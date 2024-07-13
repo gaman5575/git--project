b9f58d36ccf4c5674901006546504685fa0b74be
Aman kumar <157531627+gaman5575@users.noreply.github.com>
2024-07-13 18:49:48 +0530

    Update jenkins-trigger.py

--- jenkins-trigger.py
+++ jenkins-trigger.py
@@ -5,9 +5,10 @@ import sys
 jenkins_user = os.getenv('JENKINS_USER')
 jenkins_token = os.getenv('JENKINS_TOKEN')
 jenkins_urls = [
-    "http://192.168.125.147:8080/job/python-project-1/build",
+    #"http://192.168.125.147:8080/job/python-project-1/build",
+    "http://192.168.125.147:8080/job/python-project-2/build"
     
-    # Add more URLs as needed
+    
 ]
 
 for url in jenkins_urls:
