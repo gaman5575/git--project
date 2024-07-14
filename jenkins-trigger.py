cad0e2c66c94c0c21201e5dbfad3eafe07f1ffdf
Aman kumar <157531627+gaman5575@users.noreply.github.com>
2024-07-14 12:00:50 +0530

    Update jenkins-trigger.py

--- jenkins-trigger.py
+++ jenkins-trigger.py
@@ -6,10 +6,10 @@ jenkins_user = os.getenv('JENKINS_USER')
 jenkins_token = os.getenv('JENKINS_TOKEN')
 jenkins_urls = [
     "http://192.168.125.147:8080/job/python-project-1/build",
-    "http://192.168.125.147:8080/job/python-project-2/build",
-    "http://192.168.125.147:8080/job/python-project-3/build",
-    "http://192.168.125.147:8080/job/python-project-4/build",
-    "http://192.168.125.147:8080/job/python-project-5/build"
+    #"http://192.168.125.147:8080/job/python-project-2/build",
+    #"http://192.168.125.147:8080/job/python-project-3/build",
+    #"http://192.168.125.147:8080/job/python-project-4/build",
+    #"http://192.168.125.147:8080/job/python-project-5/build"
     
     
 ]
